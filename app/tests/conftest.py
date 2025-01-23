a4cf7a2b6d972e07a38ca437c0fc19679b4d0b22
Moez <moez.saidi@wintics.com>
2025-01-23 14:44:22 +0100

    fix: debug database rollback in tests

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -3,11 +3,14 @@ import asyncio
 import pytest
 from fastapi.testclient import TestClient
 from httpx import ASGITransport, AsyncClient
+from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 from app.core.database import DATABASE_URI, Base
+from app.core.hashing import get_password_hash
 from app.main import app
+from app.models.users import User
 
 
 @pytest.fixture(scope="session")
@@ -23,16 +26,14 @@ def anyio_backend():
 
 
 @pytest.fixture(scope="session")
-async def create_test_database():
-    engine = create_async_engine(DATABASE_URI, echo=True)
+async def db_engine():
+    engine = create_async_engine(DATABASE_URI)
 
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
-    async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-
-    yield async_session_maker
+    yield engine
 
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
@@ -41,19 +42,51 @@ async def create_test_database():
 
 
 @pytest.fixture(scope="function")
-async def db_session(create_test_database):
-    async_session_maker = create_test_database
+async def db_session(db_engine):
+    async_session_maker = sessionmaker(
+        bind=db_engine,
+        class_=AsyncSession,
+        expire_on_commit=False,
+        autocommit=False,
+        autoflush=False,
+    )
+
     async with async_session_maker() as session:
+        await session.begin()
         yield session
         await session.rollback()
+        for table in reversed(Base.metadata.sorted_tables):
+            await session.execute(text(f"TRUNCATE {table.name} CASCADE;"))
+            await session.commit()
 
 
 @pytest.fixture(scope="function")
 async def client():
-    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test/api") as ac:
         yield ac
 
 
 @pytest.fixture(scope="function")
 def sync_client():
     return TestClient(app)
+
+
+@pytest.fixture(scope="function")
+async def create_test_users(db_session: AsyncSession):
+    async def _create_test_users(num_users: int = 3):
+        users = []
+        for i in range(num_users):
+            user = User(
+                username=f"testuser{i}",
+                email=f"testuser{i}@example.com",
+                hashed_password=get_password_hash(f"password{i}"),
+                is_active=True,
+            )
+            db_session.add(user)
+            users.append(user)
+
+        await db_session.commit()
+
+        return users
+
+    yield _create_test_users
--- app/tests/test_users.py
+++ app/tests/test_users.py
@@ -4,14 +4,49 @@ import string
 import pytest
 from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from app.models.users import User
 
 
 @pytest.mark.asyncio
-async def test_create_user(async_test_client: AsyncClient, db_session: AsyncSession):
+async def test_create_user(client: AsyncClient, db_session: AsyncSession) -> None:
     user_data = {
         "username": "Momo",
         "email": "user@example.com",
         "password": "".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10)),
     }
-    response = await async_test_client.post("/users/", json=user_data)
+    response = await client.post("/users/", json=user_data)
     assert response.status_code == 200
+    db_user = await db_session.execute(select(User).where(User.email == user_data["email"]))
+    db_user = db_user.scalar_one_or_none()
+
+    assert db_user is not None
+    assert db_user.username == user_data["username"]
+    assert db_user.email == user_data["email"]
+
+
+@pytest.mark.asyncio
+async def test_list_users(client: AsyncClient, create_test_users):
+    test_users = await create_test_users(4)
+    response = await client.get("/users/")
+    assert response.status_code == 200
+    users = response.json()
+    assert isinstance(users, list)
+    assert len(users) == len(test_users)
+
+
+# @pytest.mark.asyncio
+# async def test_login(client: AsyncClient, create_test_users):
+#     await create_test_users(1)
+#     response = await client.get("/users/")
+#     assert response.status_code == 200
+#     print(response.json())
+#     login_data = {
+#         "email": "testuser0@example.com",
+#         "password": "password0",
+#     }
+#     response = await client.post("/login", data=login_data)
+#     assert response.status_code == 200
+#     assert response.json() == {}
+#     assert "access_token" in response.json()
